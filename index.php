b3f6dd5fccb919a4210cf2b5533042f2b7f427d4
Robin Bron <robin@vpscash.nl>
2016-04-01 14:11:44 +0200

    Allow for plugins now

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+// include plugins
+foreach(glob('plugin/*.php') as $file) {
+    include($file);
+}
+
+// and adminer itself
 $files = glob('adminer-*.php');
 sort($files);
-
 include array_pop($files);
-
