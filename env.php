5a01c18662119295d99a391c6d7643f8d5912ada
Robin Bron <robin@finwo.nl>
2016-11-29 10:37:45 +0100

    Env easier to read

--- env.php
+++ env.php
@@ -1,5 +1,31 @@
 <?php
 
+function longest($input) {
+    $l = 0;
+    foreach ($input as $item) {
+        $l = max($l, strlen($item));
+    }
+    return $l;
+}
+
 print("<pre>");
-var_export($_ENV);
+$keys = array_keys($_ENV);
+sort($keys);
+$len=longest($keys);
+printf("%s\n\n", count($keys));
+foreach ($keys as $key) {
+    printf("%s _%s => (%s) ", $key, str_repeat('_', $len-strlen($key)), gettype($_ENV[$key]));
+    switch (gettype($_ENV[$key])) {
+        case 'object':
+            $_ENV[$key] = (array)$_ENV[$key];
+        case 'array':
+            print(http_build_query($_ENV[$key]));
+            print("\n");
+            break;
+        default:
+            print($_ENV[$key]);
+            print("\n");
+            break;
+    }
+}
 print("</pre>");
