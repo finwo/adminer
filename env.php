20b517ae556989636f32ff3d892b0d940bff3770
Robin Bron <robin@finwo.nl>
2016-11-29 10:25:45 +0100

    Simpler dumping

--- env.php
+++ env.php
@@ -1,22 +1,5 @@
 <?php
 
-function prnt($element) {
-    $type = gettype($element);
-    printf('<ul style="list-style:none;">');
-    switch(gettype($element)) {
-        case 'object':
-            $type = get_class($element);
-            $element = (array)$element;
-        case 'array':
-            printf("<li><b>%s</b>");
-            array_walk($element, 'prnt');
-            printf("</li>");
-            break;
-        default:
-            printf("<li><b>%s</b><p>%s</p></li>", $type, $element);
-            break;
-    }
-    printf('</ul>');
-}
-
-prnt($_ENV);
+print("<pre>");
+var_export($_ENV);
+print("</pre>");
