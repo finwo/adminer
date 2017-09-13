69b0a8ca038117ce66711d8d3684661672d088e1
Robin Bron <robin@finwo.nl>
2017-09-13 12:07:19 +0200

    Added dump-json plugin

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
 
-// include plugins
-$files = glob('plugins/*.php');
-sort($files);
-foreach($files as $file) {
-    include($file);
+function adminer_object() {
+
+  // Required
+  include_once "./plugins/plugin.php";
+
+  // Autoloader
+  foreach(glob("./plugins/*.php") as $file) {
+    include_once $filename;
+  }
+
+  // Select plugins to enable
+  $plugins = array(
+    new AdminerDumpJson(),
+  );
+
+  // Enable the chosen plugins
+  return new AdminerPlugin($plugins);
 }
 
 // and adminer itself
