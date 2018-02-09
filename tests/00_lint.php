98c304977bc61d9fcec2ed87242f5204a0dd8ced
Robin Bron <robin@finwo.nl>
2018-02-09 10:36:40 +0100

    Added basic travis test

---
+++ tests/00_lint.php
@@ -0,0 +1,32 @@
+<?php
+
+// Check all php files for syntax errors
+
+$test = Test::init('PHP file linting');
+
+foreach(scandir_recursive(APPROOT) as $filename) {
+    if(strpos($filename,'/.')!==false) continue;
+    if(strpos($filename,APPROOT.DS.'vendor'.DS)!==false) continue;
+    $extension = explode('.', $filename);
+    $extension = array_pop($extension);
+    if(!in_array($extension, array('inc','php'))) continue;
+    $test->assertContains('No syntax errors', exec(sprintf('php -l "%s" 2>/dev/null', $filename), $out), sprintf("%s contains syntax errors", $filename));
+}
+
+// Check all json files for syntax errors
+
+$test = Test::init('JSON file linting');
+
+foreach(scandir_recursive(APPROOT) as $filename) {
+    if(strpos($filename,'/.')!==false) continue;
+    if(strpos($filename,APPROOT.DS.'vendor'.DS)!==false) continue;
+    $extension = explode('.', $filename);
+    $extension = array_pop($extension);
+    if(!in_array($extension, array('json'))) continue;
+    try {
+        $object = json_decode(file_get_contents($filename));
+        $test->assertNot(null, $object, sprintf("%s does not contain valid JSON", $filename));
+    } catch(Exception $e) {
+        $test->assert(true, false, sprintf("Parsing error during %s", $filename));
+    }
+}
