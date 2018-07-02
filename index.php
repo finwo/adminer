7c16420465dd21ad44f0ba9bc55a15aaf8451942
Robin Bron <robin@finwo.nl>
2018-07-02 11:08:59 +0200

    Auto-detect plugins in folder

--- index.php
+++ index.php
@@ -2,24 +2,51 @@
 
 function adminer_object() {
 
-  // Required
-  include_once "./plugins/plugin.php";
+    // Required
+    require_once "./plugins/plugin.php";
 
-  // Autoloader
-  foreach(glob("./plugins/*.php") as $file) {
-    include_once $file;
-  }
+    // Helper functions
+    function file_get_php_classes($filepath) {
+        $php_code = file_get_contents($filepath);
+        $classes = get_php_classes($php_code);
+        return $classes;
+    }
 
-  // Select plugins to enable
-  $plugins = array(
-    new AdminerDumpJson(),
-  );
+    function get_php_classes($php_code) {
+        $classes = array();
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if ($tokens[$i - 2][0] == T_CLASS
+                && $tokens[$i - 1][0] == T_WHITESPACE
+                && $tokens[$i][0] == T_STRING) {
 
-  // Enable the chosen plugins
-  return new AdminerPlugin($plugins);
+                $class_name = $tokens[$i][1];
+                $classes[] = $class_name;
+            }
+        }
+        return $classes;
+    }
+
+    // Fetch all plugins
+    $plugins = array();
+    foreach (glob("./plugins/*.php") as $file) {
+        if($file=='./plugins/plugin.php') continue;
+        $classes = file_get_php_classes($file);
+        require_once($file);
+        $plugins = array_merge($plugins,$classes);
+    }
+
+  // Enable the found plugins
+  return new AdminerPlugin(array_map(function($class) {
+      return new $class();
+  }, $plugins));
 }
 
-// and adminer itself
-$files = glob('adminer-*.php');
-sort($files);
-include array_pop($files);
+
+// Load the most recent adminer version
+require (function () {
+    $files = glob('adminer-*.php');
+    sort($files);
+    return array_pop($files);
+})();
